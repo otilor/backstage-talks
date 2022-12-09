72ac36ddd59aeee1102039299613e7e53e90c65a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-09 15:36:16 +0100

    format

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,20 +11,27 @@ export default function Home() {
       </Head>
 
       <main>
-          <Image className='px-4 py-5' src="/images/logo.png" alt="Cover" width={300} height={1000} />
-          <div className='fixed top-0 right-0'>info@backstagetalks.com</div>
-          <div className='fixed bottom-0 w-64 px-5 font-bold left-0 whitespace-normal'>
-          Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right ones, we need to talk.
-          </div>
-          <div className='fixed bottom-0 right-0 w-16'>
-            <p>Issue #6</p>
-            <p>Issue #6</p>
-            <p>Issue #6</p>
-            <p>Issue #6</p>
-            <p>Issue #6</p>
-          </div>
+        <Image
+          className="px- py-5"
+          src="/images/logo.png"
+          alt="Cover"
+          width={300}
+          height={1000}
+        />
+        <div className="fixed top-0 right-0">info@backstagetalks.com</div>
+        <div className="fixed bottom-0 w-64 px-5 font-bold left-0 whitespace-normal">
+          Backstage Talks is a magazine of casual, but in depth dialogues on
+          design and business. Our decisions shape and influence this complex
+          world—to have a chance to make the right ones, we need to talk.
+        </div>
+        <div className="fixed bottom-0 right-0 w-16">
+          <p>Issue #6</p>
+          <p>Issue #6</p>
+          <p>Issue #6</p>
+          <p>Issue #6</p>
+          <p>Issue #6</p>
+        </div>
       </main>
-
     </div>
-  )
+  );
 }
