dcb9a9531ca1b7c160c83905077f993d2ae5f331
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-09 15:30:39 +0100

    add padding

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-          <div className="fixed top-0 left-0 h-16 w-16">Contacts</div>
-          <Image src="/images/logo.png" alt="Cover" width={280} height={1000} />
+          <Image className='p-4' src="/images/logo.png" alt="Cover" width={280} height={1000} />
           <div className='fixed bottom-0 w-64 left-0 whitespace-normal'>
           Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right ones, we need to talk.
           </div>
